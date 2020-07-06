d97ccd416d2d6972b9596d976837002025238bbc
majhogee <jo.wojtowicz@gmail.com>
2020-07-06 23:39:42 +0200

    Use react-posenet and display positions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import PoseNet from "react-posenet";
 import './App.css';
 
 function App() {
+  const [posesString, setPosesString] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>PoseNet</h1>
+        <PoseNet inferenceConfig={{ decodingMethod: "single-person" }}
+                 onEstimate={poses => {setPosesString(JSON.stringify(poses))}}/>
+        <p>{posesString}</p>
     </div>
   );
 }
